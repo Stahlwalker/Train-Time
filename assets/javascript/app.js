22615bd6b04f79ab90766c3715abeaf9c075178a
Luke Stahl <Stahl1109@gmail.com>
2018-01-18 13:39:20 -0600

    API update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,7 +20,7 @@
 
   function displayTrainInfo() {
 // transport API
-  var queryURL = "http://transportapi.com/v3/uk/places.json?query=euston&type=train_station&app_id=YOUR_APP_ID&app_key=914c74173c980e2f0faa659c14e6a714";
+  var queryURL = "http://transportapi.com/v3/uk/places.json?query=euston&type=train_station&app_id=5bc5ba12&app_key=914c74173c980e2f0faa659c14e6a714";
   console.log(queryURL);
 
   $.ajax({
