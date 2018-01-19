ea9aa6b817b308098146010c0a19d73f3846e9ee
Luke Stahl <Stahl1109@gmail.com>
2018-01-19 08:32:50 -0600

    console log train time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,55 @@
-
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyAaUycNT-YHVYTFyGlu4A0-zCyisaubFdo",
     authDomain: "train-time-eb924.firebaseapp.com",
     databaseURL: "https://train-time-eb924.firebaseio.com",
     projectId: "train-time-eb924",
     storageBucket: "",
     messagingSenderId: "595268866699"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
 
-	// Creates local "temporary" object for holding train data
-	// var newTrain = {
-	// 	name: name,
-	// 	destination: destination,
-	// 	firstTrain: firstTrainUnix,
-	// 	frequency: frequency
-	// }
+// Creates local "temporary" object for holding train data
+// var newTrain = {
+// 	name: name,
+// 	destination: destination,
+// 	firstTrain: firstTrainUnix,
+// 	frequency: frequency
+// }
 
-    var name = "";
-    var destination = "";
-    var firstTrain = "";
-    var frequency = "";
-  
+var name = "";
+var destination = "";
+var firstTrain = "5:30";
+var frequency = 5;
+
+var firstTimeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
+console.log(firstTimeConverted);
+
+var currentTime = moment();
+console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+// Difference between the times
+var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+console.log("DIFFERENCE IN TIME: " + diffTime);
+
+// Time apart (remainder)
+var tRemainder = diffTime % frequency;
+console.log(tRemainder);
+
+// Minute Until Train
+var tMinutesTillTrain = frequency - tRemainder;
+console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+// Next Train
+var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
 
 // Click Button changes what is stored in firebase
-$("#submit").on("click", function() {
+$("#submit").on("click", function () {
     // Prevent the page from refreshing
     event.preventDefault();
 
@@ -37,39 +58,64 @@ $("#submit").on("click", function() {
     destination = $("#search-destination").val().trim();
     firstTrain = $("#search-time").val().trim();
     frequency = $("#search-frequency").val().trim();
-    
+
 
     // Change what is saved in firebase
-    database.ref().push({
-      name: name,
-      destination: destination,
-      firstTrain: firstTrain,
-      frequency: frequency
+    database.ref().set({
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        //   dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
-  });
+});
 
-  // Firebase is always watching for changes to the data.
-  // When changes occurs it will print them to console and html
-  database.ref().on("value", function(snapshot) {
+// Firebase is always watching for changes to the data.
+// When changes occurs it will print them to console and html
+database.ref().on("value", function (snapshot) {
+    // database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (snapshot) {
 
+    // var sv = snapshot.val();
     // Print the initial data to the console.
     console.log(snapshot.val());
 
+
+    // // Console.loging the last user's data
+    // console.log(sv.name);
+    // console.log(sv.destination);
+    // console.log(sv.firstTrain);
+    // console.log(sv.frequency);
     // Log the value of the various properties
     console.log(snapshot.val().name);
     console.log(snapshot.val().destination);
     console.log(snapshot.val().firstTrain);
     console.log(snapshot.val().frequency);
 
+    //   Change the HTML to reflect
+    $("#search-name").text(name);
+    $("#search-destination").text(destination);
+    $("#search-time").text(firstTrain);
+    $("#search-frequency").text(frequency);
+    // $("").text(tMinutesTillTrain);
+    // $("").text(nextTrain);
+
     // Change the HTML
-    $(".train-info").text(snapshot.val().name + " | " + snapshot.val().destination + " | " + snapshot.val().firstTrain + " | " + snapshot.val().frequency);
+    $(".train-info").text(snapshot.val().name + " | " + snapshot.val().destination + " | " + snapshot.val().firstTrain + " | " + snapshot.val().frequency + " | " + snapshot.val().tMinutesTillTrain + " | " + snapshot.val().nextTrain);
 
     // If any errors are experienced, log them to console.
-  }, function(errorObject) {
-    console.log("The read failed: " + errorObject.code);
-  });
+}, function (errorObject) {
+    console.log("Errors handled " + errorObject.code);
+});
 
-
+//   var table = document.getElementsByTagName(".train-info")[0];
+//         database.ref().on("child_added", function (child) {
+//             console.log(child.key + ': ' + child.val());
+//             var tr = document.createElement('tr');
+//             var td = document.createElement('td');
+//             td.innerText = child.val().name + " --- " + JSON.stringify(child.val());
+//             tr.appendChild(td);
+//             table.appendChild(tr);
+//         });
 
 
 
@@ -131,7 +177,7 @@ $("#submit").on("click", function() {
 //     console.log($("#submit").val());
 
 //     var userInput = $("#submit").val().trim();
-  
+
 //     // console.log("Current: " + topics);
 //     tRow.push(userInput);
 //     // console.log("New Array: " + topics); 
